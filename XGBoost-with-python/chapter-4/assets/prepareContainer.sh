816644c640c7a8d8585f31a122b6b0fad0abdc0d
khalil <malikhalil36@gmail.com>
2019-10-19 12:10:15 +0500

    first commit

--- XGBoost-with-python/chapter-4/assets/prepareContainer.sh
+++ XGBoost-with-python/chapter-4/assets/prepareContainer.sh
@@ -1,38 +1,29 @@
-pip install --upgrade pip
-# git clone https://github.com/kavgan/word_cloud 
-# cd word_cloud && pip install -e . 
-# cp  ~/word_cloud/*clouds.ipynb  ~/work/Example_word_clouds.ipynb
-pip install -U xgboost
-pip install -U keras
-pip install plotly 
-pip install matplotlib_venn 
-pip install cufflinks 
-pip install gender-guesser
-pip install -U nltk
-# # apparently the base image already contain ipywidgets
-# #pip install ipywidgets
-pip install pyarrow
-# # word_cloud has a fixed dependency on an older version of pandas - and when installed will overwrite the current one that comes in the Docker image
-# # here I am force installing the latest pandas - required by the widgets notebook; word_cloud will still work
-# pip install --upgrade pandas
-# cd ~/work
-# git clone http://bitbucket.org/hrojas/learn-pandas
-# # adding notebook to play with ipywidgets
-# git clone https://github.com/WillKoehrsen/Data-Analysis
-# cp -R Data-Analysis/widgets ~/work
-# mkdir temp
-# cd temp
-# # next follows a round about way to get a single file into the running container
-# git clone https://github.com/lucasjellema/katacoda-scenarios
-# cp katacoda-scenarios/jupyter-notebook/assets/introPythonForDataAnalysis.ipynb ~/work
-# # clone the data wrangling workshop github repo
-# git clone https://github.com/lucasjellema/DataAnalytics--IntroductionDataWrangling-JupyterNotebooks
-# # then move folder CaseOfOracleOpenWorld2018 under ~/work
-# mv DataAnalytics--IntroductionDataWrangling-JupyterNotebooks/CaseOfOracleOpenWorld2018 ~/work
-# # and move folder HelloWorldNotebook under ~/work
-# mv DataAnalytics--IntroductionDataWrangling-JupyterNotebooks/HelloWorldNotebook ~/work
-# # and move folder Titanic under ~/work
-# mv DataAnalytics--IntroductionDataWrangling-JupyterNotebooks/Titanic ~/work
+# pip install --upgrade pip
+# pip install matplotlib_venn 
 
-# cd ~/work
-# rm -Rf temp 
+
+# pip install keras
+# pip install Pillow
+
+echo 'Installing Dependencies'
+
+pip install xgboost
+
+pip install opencv-python
+pip install mtcnn
+pip install numpy==1.16.0
+
+pip install pydot
+pip install graphviz
+
+pip install git+https://github.com/rcmalli/keras-vggface.git
+
+sudo yes | add-apt-repository ppa:gviz-adm/graphviz-dev
+sudo apt-get update
+sudo yes | apt-get install graphviz-dev
+
+# conda install theano
+# conda install -c conda-forge tensorflow
+# conda install graphviz
+# conda install pydotplus -y
+conda install graphviz -y
--- XGBoost-with-python/chapter-4/set-env.sh
+++ XGBoost-with-python/chapter-4/set-env.sh
@@ -13,14 +13,10 @@ docker cp  ~/tutorial/. jupyter:/home/jovyan/work
 # copy the script to make it accessible and executable 
 docker exec -it jupyter bash -c 'cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh'
 # execute the script inside the container - to install a number of packages
-# docker exec -it jupyter bash -c './prepareContainer.sh'
-docker exec -it jupyter bash -c 'assets/prepareContainer.sh'
-pip install -U xgboost
-pip install -U keras
+docker exec -it jupyter bash -c './prepareContainer.sh'
 # # restart the docker container
 docker restart jupyter
 echo 'restarted Jupyter container'
-echo "this is new environment"
 # perhaps clone a GitHub repo inside the Jupyter container
 #docker exec -it jupyter git clone https://github.com/lucasjellema/DataAnalytics--IntroductionDataWrangling-JupyterNotebooks
 # and more stuff?
