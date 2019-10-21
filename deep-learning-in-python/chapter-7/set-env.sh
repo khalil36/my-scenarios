fef4398e91b360550cc9d42b9976962f045597ee
khalil <malikhalil36@gmail.com>
2019-10-21 18:17:26 +0500

    commit

--- deep-learning-in-python/chapter-7/set-env.sh
+++ deep-learning-in-python/chapter-7/set-env.sh
@@ -1,11 +1,11 @@
 docker run -d --user root -p 8888:8888 --name jupyter -e GRANT_SUDO=yes jupyter/tensorflow-notebook:2ce7c06a61a1 start-notebook.sh
 
-git clone https://github.com/khalil36/deep-learning.git
+git clone https://github.com/akramsahmad/deep-learning-in-python.git
 
 # copy the script into the container; note: the resulting file is owned by root and it not executable
-docker cp ~/tutorial/deep-learning/prepareContainer.sh jupyter:/tmp/prepareContainerRoot.sh
+docker cp ~/tutorial/deep-learning-in-python/prepareContainer.sh jupyter:/tmp/prepareContainerRoot.sh
 
-docker cp  ~/tutorial/deep-learning/chapter-7/. jupyter:/home/jovyan/work
+docker cp  ~/tutorial/deep-learning-in-python/chapter-7/. jupyter:/home/jovyan/work
 
 # copy the script to make it accessible and executable 
 docker exec -it jupyter bash -c 'sudo chmod -R 777 ~ && cp /tmp/prepareContainerRoot.sh ~/prepareContainer.sh && chmod +x ~/prepareContainer.sh && ~/prepareContainer.sh'
